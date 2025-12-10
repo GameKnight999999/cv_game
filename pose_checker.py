a5cb0695d529070229e549af2cb1c7a3e4bf83cf
Stepan Blinov <blinov.school@yandex.ru>
2025-12-10 06:13:12 +0300

    Added typing
    Added typing for pose checking functions

--- pose_checker.py
+++ pose_checker.py
@@ -1,13 +1,14 @@
 import math, json, os
 from settings import *
+from typing import Sequence
 
 
-def check_id(pose, sample_id) -> float:
+def check_id(pose: Sequence, sample_id: int) -> float:
     sample = json.load(open(os.path.sep.join((SAMPLES_PATH, f"{sample_id:02d}.json")), encoding="utf-8"))
     return check_sample(pose, sample)
 
 
-def check_sample(pose, sample) -> float:
+def check_sample(pose: Sequence, sample: Sequence) -> float:
     conns = [math.atan2(pose[p1][1] - pose[p2][1], pose[p1][0] - pose[p2][0]) for p1, p2 in ((16, 14), (14, 12), (11, 13), (13, 15), 
                                                                                              (28, 26), (26, 24), (23, 25), (25, 27))]
     cost = 0
