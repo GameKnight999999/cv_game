db353e6d916c9027735a7c6988f817d96d88d83a
Stepan Blinov <blinov.school@yandex.ru>
2025-12-18 07:23:08 +0300

    Started working on main loop

--- main.py
+++ main.py
@@ -1,29 +1,42 @@
 from settings import *
+import ui_facade as ui
 
 
-MAIN_MENU, SETTINGS, HELP, GAME_SETUP, ROUND_SHOW_POSE, ROUND_TIMER, ROUND_SHOW_SCORE, TOTAL_RATING, QUIT = range(9)
+MAIN_MENU, SETTINGS, HELP, GAME_SETUP, ROUND_SHOW_POSE, ROUND_TIMER, ROUND_SHOW_SCORE, TOTAL_RATING, QUIT, NULL = range(10)
+
+
+def clearscreen() -> None:
+    ui.ui_elements = []
+
+
+def main_menu() -> None:
+    clearscreen()
+    ui.Image.load(MAIN_MENU_BACKGROUND, 0, 0)
+    
 
 
 def main() -> None:
-    global status
-    status = MAIN_MENU
-    while status != QUIT:
-        if status == MAIN_MENU:
+    global event
+    event = MAIN_MENU
+    while event != QUIT:
+        if event == MAIN_MENU:
+            main_menu()
+        elif event == SETTINGS:
             pass
-        elif status == SETTINGS:
+        elif event == HELP:
             pass
-        elif status == HELP:
+        elif event == GAME_SETUP:
             pass
-        elif status == GAME_SETUP:
+        elif event == ROUND_SHOW_POSE:
             pass
-        elif status == ROUND_SHOW_POSE:
+        elif event == ROUND_TIMER:
             pass
-        elif status == ROUND_TIMER:
+        elif event == ROUND_SHOW_SCORE:
             pass
-        elif status == ROUND_SHOW_SCORE:
+        elif event == TOTAL_RATING:
             pass
-        elif status == TOTAL_RATING:
-            pass
+        event = NULL
+        ui.tick()
 
 
 if __name__ == "__main__":
--- settings.py
+++ settings.py
@@ -1,2 +1,5 @@
 SAMPLES_PATH = "poses"
 VIDEO_PLACEHOLDER = "no_camera.jpg"
+
+# Backgrounds
+MAIN_MENU_BACKGROUND = ""
