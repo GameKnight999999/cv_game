d19cc3ed602ab8e73013349aca100b135f193265
Stepan Blinov <blinov.school@yandex.ru>
2025-12-19 07:19:41 +0300

    Added button rendering
    Added rendering function to button and modified a bit Font class

--- ui_facade.py
+++ ui_facade.py
@@ -114,6 +114,68 @@ class Font:
         if message:
             print('Some of symbols in your text are not available yet, check out the file')
 
+
+    def print_at(self, text: str, size: float, x: int, y: int) -> None:
+        """
+        Prints the text with left top corner at (x, y)
+        
+        :param text: Text to print
+        :param size: Font size
+        :param x: Left-top corner x coord
+        :param y: Left-top corner y coord
+        """
+        """
+        Prints the given text on the screen and writes not available symbols in a file
+        Args:
+            text: the text that will be printed out
+            symbol_size: the size of each symbol in pixels, a set with two variables: width and height
+        Returns:
+            A string containing the greeting message.
+        """
+        #basic stuff -- getting symbol size, the max amount of symbols the screen can fit, turning text into capitals, setting symbol index to 0
+        symbol_width, symbol_height = size, size * 1.5
+        line_max_symbols = screen_width // symbol_width - 1
+        text = text.upper()
+        index = 0
+        message = False
+
+        #if the text won't fit in the screen
+        start_text_pos_x = x
+
+        #getting the storting position
+        start_pos = (start_text_pos_x, y + symbol_height * self.line)
+
+        #processing the text ig
+        for symbol in text:
+            if symbol in self.available_symbols:
+                # the system didn't let me save files named like :.png, etc, so i added this
+                # please don't delete or it's gonna crash :(
+                if symbol == '.':
+                    symbol = 'dot'
+                elif symbol == ':':
+                    symbol = 'colon'
+                elif symbol == ' ':
+                    symbol = 'space'
+                # print the symbol, change the x
+                screen.blit(load_image(f'{symbol}.png'), start_pos)
+                start_text_pos_x += symbol_width
+                # move to the next line
+                if index == line_max_symbols:
+                    self.line += 1
+                    start_text_pos_x = x
+                    index -= line_max_symbols
+            else:
+                message = True
+                self.not_available.add(symbol)
+            # change the position and increase the symbol's index by one
+            start_pos = (start_text_pos_x, self.start_text_pos_y + symbol_height * self.line)
+            index += 1
+
+        self.line = 0
+        if message:
+            print('Some of symbols in your text are not available yet, check out the file')
+
+
     def start_timer(self, start_time) -> None:
         """
         Starts a decreasing timer
@@ -122,6 +184,7 @@ class Font:
         """
         self.seconds = [str(sec) for sec in range(0, start_time + 1)]
         self.timer_running = True
+
 
     def end_timer(self) -> None:
         """
@@ -233,6 +296,7 @@ class Button(UIElement):
         self.action = action
         self.x, self.y = x, y
         self.width, self.height = width, height
+        self.font = Font()
         def callback(e: pygame.event.Event):
             if 0 < e.pos[0] - x < self.width and 0 < e.pos[1] - x < self.height:
                 self.action()
@@ -244,7 +308,8 @@ class Button(UIElement):
         """
         Render the button to the screen
         """
-        pass
+        pygame.draw.rect(screen, "green", (self.x, self.y, self.width, self.height), border_radius=5)
+        self.font.print_at(self.text, 14, self.x, self.y)
 
 
     def __del__(self) -> None:
