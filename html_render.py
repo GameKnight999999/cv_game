00ba541354e55b5f2cca881ff5c56c37de300865
Stepan Blinov <blinov.school@yandex.ru>
2025-12-21 21:56:43 +0300

    Fixed some bugs
    Now it shows a pose image and works pretty fine

--- html_render.py
+++ html_render.py
@@ -1,23 +1,11 @@
 import os, json
 import cv2, webview
-from pose_checker import check_frame
 from settings import *
-
-
-MAIN_MENU, SETTINGS, HELP, GAME_SETUP, ROUND_SHOW_POSE, ROUND_TIMER, ROUND_SHOW_SCORE, TOTAL_RATING, QUIT, NULL = range(10)
 
 
 class Api:
     def close(self) -> None:
         window.destroy() # type: ignore
-    
-
-    def get_frame(self):
-        if cap.isOpened():
-            success, frame = cap.read()
-            if success:
-                return cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        return cv2.imread(VIDEO_PLACEHOLDER)
     
 
     def submit(self, formHTML) -> None:
@@ -26,7 +14,10 @@ class Api:
         for node in formHTML["childNodes"]:
             if node["nodeName"] == "INPUT":
                 values[node["name"]] = node["value"]
-        getattr(self, action)(values)
+        try:
+            getattr(self, action)(values)
+        except AttributeError:
+            return
     
 
     def save(self, values: dict) -> None:
