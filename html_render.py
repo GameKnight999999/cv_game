76aab1b37c8761b6fa603838f34d9e6d06eb8ec2
Stepan Blinov <blinov.school@yandex.ru>
2025-12-22 13:30:41 +0300

    Fixed settings saving
    Fixed settings saving and added function to debug javascript

--- html_render.py
+++ html_render.py
@@ -83,7 +83,8 @@ class Api:
 
 
 def js_debug(js: str) -> None:
-    result = window.evaluate_js(f"""
+    result = window.evaluate_js( # type: ignore
+    f"""
         (() => try {{ {js}\nreturn {{ok: true}} }} catch (e) {{ return {{ok: false, message: e.message, stack: e.stack}} }})
     """)
     if not result["ok"]:
