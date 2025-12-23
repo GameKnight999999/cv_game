63aacd40d85589d3e2ff38ed6b99b9e46be99d89
Fmaj2 <mar.m.dmitrieva@gmail.com>
2025-12-23 11:52:04 +0300

    Made a fully working singleplayer mode
    Added all the menus -- settings, game setup, rounds, stats and the final
    ranking
    Added a possibility to adjust the timer in the settings menu and view
    highscore
    Added a possibility to adjust the amount of rounds in the game setup
    menu
    Everything works, but minor bugs are possible

--- main.py
+++ main.py
@@ -1,37 +1,174 @@
-import random
-from settings import *
+from random import randint
+import pygame
+import cv2
+
+import event_handler
+from pose_checker import *
+import mediapipe as mp
+import settings
 import ui_facade as ui
 
 
 MAIN_MENU, SETTINGS, HELP, GAME_SETUP, ROUND_SHOW_POSE, ROUND_TIMER, ROUND_SHOW_SCORE, TOTAL_RATING, QUIT, NULL = range(10)
+music_playing = False
+score = []
+font = ui.Font()
+round_num = 0
+round_score = 0
 
 def test():
     ui.play_sound('boop')
-'''
-def mental_help() -> None:
-    ui.clearscreen()
-    ui.Image.load('test.png', 100, 100)
-    ui.Button("hewwo :3", lambda: test(), 200, 800, 200, 100)
-'''
+
 def help() -> None:
     ui.clearscreen()
-    ui.Button("follow the pose of the", lambda: test(), 200, 100, 200, 100)
-    ui.Button("little guy on the screen", lambda: test(), 200, 200, 200, 100)
-    ui.Button("as precise as you can", lambda: test(), 200, 300, 200, 100)
-    ui.Button("to get more points and ", lambda: test(), 200, 400, 200, 100)
-    ui.Button("win the round!", lambda: test(), 200, 500, 200, 100)
-    ui.Button("go back", lambda: globals().__setitem__("event", MAIN_MENU), 500, 800, 200, 100)
+    ui.Button("follow the pose of the", lambda: test(), 200, 100, False)
+    ui.Button("little guy on the screen", lambda: test(), 200, 200, False)
+    ui.Button("as precise as you can", lambda: test(), 200, 300, False)
+    ui.Button("to get more points and ", lambda: test(), 200, 400, False)
+    ui.Button("win the round!", lambda: test(), 200, 500, False)
+    ui.Button("go back", lambda: globals().__setitem__("event", MAIN_MENU), 500, 800, True)
+
+def draw_settings():
+    ui.clearscreen()
+    ui.ButtonArrows('timer value:', 'TIMER', settings.TIMER, 60, 50, 50)
+    ui.Button("done", lambda: globals().__setitem__("event", MAIN_MENU), 500, 800, True)
+    if settings.HIGH_SCORE:
+        ui.Label(f'{settings.HIGH_SCORE}', settings.FONT_SIZE, 50, 250)
 
 
 def main_menu() -> None:
+    global music_playing
     ui.clearscreen()
     ui.Image.load(MAIN_MENU_BACKGROUND, 0, 0)
-    ui.Button("Start game", lambda: globals().__setitem__("event", GAME_SETUP), 50, 50, 200, 100)
-    ui.Button("Quit", lambda: globals().__setitem__("event", QUIT), 50, 200, 200, 100)
-    ui.Button("Settings", lambda: globals().__setitem__("event", SETTINGS), 50, 350, 200, 100)
-    ui.Button("Help", lambda: globals().__setitem__("event", HELP), 50, 500, 200, 100)
-    ui.play_music('test')
+    ui.Button("Start game", lambda: globals().__setitem__("event", GAME_SETUP), 50, 50, True)
+    ui.Button("Quit", lambda: globals().__setitem__("event", QUIT), 50, 200, True)
+    ui.Button("Settings", lambda: globals().__setitem__("event", SETTINGS), 50, 350, True)
+    ui.Button("Help", lambda: globals().__setitem__("event", HELP), 50, 500, True)
+    if not music_playing:
+        ui.play_music('test')
+        music_playing = True
 
+def game_setup():
+    ui.clearscreen()
+    buttons_or_smth = []
+    buttons_or_smth.append(ui.ButtonArrows('number of players:', 'PLAYERS', settings.PLAYERS, settings.MAX_PLAYERS, 50, 50))
+    buttons_or_smth.append(ui.ButtonArrows('number of rounds:', 'ROUNDS', settings.ROUNDS, settings.MAX_ROUNDS, 50, 300))
+    buttons_or_smth.append(ui.Button("continue", lambda: globals().__setitem__("event", ROUND_SHOW_POSE), 500, 800, True))
+
+
+
+
+def draw_timer():
+    global round_num
+    if font.timer_running:
+        elapsed = pygame.time.get_ticks() // 1000 - font.timer_start_time
+        remaining = settings.TIMER - elapsed
+
+        if remaining <= 0:
+            font.end_timer()
+            return
+
+        ui.Font.print_at(font, str(remaining), settings.FONT_SIZE, 500, 150)
+
+def show_round_stats():
+    global round_num, score
+    ui.clearscreen()
+    ui.Label(f'round: {round_num}', settings.FONT_SIZE, 100, 100)
+    ui.Label(f'score in this round: {score[-1]}', settings.FONT_SIZE, 100, 300)
+    ui.Label(f'total score: {sum(score)}', settings.FONT_SIZE, 100, 500)
+    ui.Button('continue', lambda: globals().__setitem__("event", ROUND_SHOW_POSE), 500, 800, True)
+
+def show_round_results():
+    global round_num, score
+    if sum(score) >= settings.HIGH_SCORE:
+        settings.HIGH_SCORE = sum(score)
+    ui.Label(f'rounds played: {round_num}', settings.FONT_SIZE, 400, 100)
+    ui.Label(f'total score {sum(score)}', settings.FONT_SIZE, 400, 300)
+    ui.Label(f'high score: {settings.HIGH_SCORE}', settings.FONT_SIZE, 400, 500)
+    ui.Button('exit', lambda: globals().__setitem__("event", MAIN_MENU), 500, 800, True)
+
+def show_round():
+    global round_score, round_num
+    round_num += 1
+    ui.clearscreen()
+    mp_drawing = mp.solutions.drawing_utils
+    mp_drawing_styles = mp.solutions.drawing_styles
+    mp_pose = mp.solutions.pose
+
+    angles = json.load(open(os.path.sep.join((SAMPLES_PATH, f"{1:02d}.json"))))
+    pose_id = randint(1, 13)
+
+    cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 900)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 600)
+
+    with mp_pose.Pose(
+            min_detection_confidence=0.5,
+            min_tracking_confidence=0.5
+    ) as pose:
+
+        font.start_timer(settings.TIMER)
+        font.timer_start_time = pygame.time.get_ticks() // 1000
+        while font.timer_running:
+            event_handler.tick()
+            ui.clearscreen()
+            draw_timer()
+            ui.Font.print_at(font, f'round: {round_num}', settings.FONT_SIZE, 450, 50)
+            success, image = cap.read()
+            if not success:
+                print("Ignoring empty camera frame.")
+                continue
+
+            image = cv2.flip(image, 1)
+
+            image.flags.writeable = False
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            results = pose.process(image)
+
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+            mp_drawing.draw_landmarks(
+                image,
+                results.pose_landmarks,
+                mp_pose.POSE_CONNECTIONS,
+                landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style()
+            )
+
+            pose_img = cv2.imread(os.path.sep.join((SAMPLES_PATH, f"{pose_id:02d}.jpg")))
+            pose_img = cv2.resize(pose_img, None, fx=1 / 5, fy=1 / 5)
+            image[image.shape[0] - pose_img.shape[0]:, :pose_img.shape[1]] = pose_img
+
+            if results and results.pose_landmarks and results.pose_landmarks.landmark:
+                conf = check_id(
+                    [(lm.x, lm.y) for lm in results.pose_landmarks.landmark],
+                    pose_id
+                )
+                cv2.putText(
+                    image,
+                    f"{conf:.4f}",
+                    (50, 50),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1,
+                    (0, 255 * conf, 255 * (1 - conf)),
+                    3
+                )
+                round_score = float(f"{conf:.4f}") * settings.MAX_SCORE
+
+            frame = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            frame = frame.swapaxes(0, 1)
+            surface = pygame.surfarray.make_surface(frame)
+
+            ui.screen.blit(surface, (200, 300))
+            pygame.display.flip()
+        if round_num < settings.ROUNDS:
+            score.append(round_score)
+            globals().__setitem__("event", ROUND_SHOW_SCORE)
+        elif round_num == settings.ROUNDS:
+            score.append(round_score)
+            globals().__setitem__("event", TOTAL_RATING)
+
+    cap.release()
 
 def main() -> None:
     global event
@@ -40,20 +177,19 @@ def main() -> None:
         if event == MAIN_MENU:
             main_menu()
         elif event == SETTINGS:
-            pass
+            draw_settings()
         elif event == HELP:
             help()
         elif event == GAME_SETUP:
-            pass
+            game_setup()
         elif event == ROUND_SHOW_POSE:
-            pass
+            show_round()
         elif event == ROUND_TIMER:
             pass
         elif event == ROUND_SHOW_SCORE:
-            pass
+            show_round_stats()
         elif event == TOTAL_RATING:
-            pass
-        event = NULL
+            show_round_results()
         ui.tick()
 
 
--- ui_facade.py
+++ ui_facade.py
@@ -1,14 +1,12 @@
 """
     UI Facade based on pygame. Gives some basic instruments like Button
 """
-
-
 import pygame
 import cv2
 import event_handler as ev
-from settings import *
 from cachetools import cached
 from typing import Callable
+import settings
 
 
 @cached(None)
@@ -29,39 +27,39 @@ def load_image(name: str) -> pygame.SurfaceType:
 
 class UIElement:
     def __init__(self) -> None:
+        self.listeners = []
         ui_elements.append(self)
-    
 
-    def render(self) -> None:
+    def destroy(self):
+        for l in self.listeners:
+            ev.remove_event_listener(l)
+
+    def render(self):
         pass
-
-
-    def __del__(self) -> None:
-        ui_elements.remove(self)
 
 
 class Font:
     """
     A font class, which can print out a text and start a timer
     """
-    def __init__(self) -> None:
-        self.available_symbols = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
-                             'S', 'T',
-                             'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', ':', ' ']
-        self.version = 'font version: 0.1'
 
-        self.symbol_size = (100, 180)
-        self.line = 0
+    def __init__(self) -> None:
+        self.available_symbols = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+                                  'R',
+                                  'S', 'T',
+                                  'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.',
+                                  ':', ' ', '<', '>']
+        self.version = 'font version: 0.1'
         self.not_available = set()
 
         self.timer_running = False
         self.seconds = []
         self.timer_start_time = None
 
-    def print_at(self, text: str, size: float, x: int, y: int) -> None:
+    def print_at(self, text: str, size: float, x: int, y: int) -> tuple:
         """
         Prints the text with left top corner at (x, y)
-        
+
         :param text: Text to print
         :param size: Font size
         :param x: Left-top corner x coord
@@ -75,17 +73,16 @@ class Font:
         Returns:
             A string containing the greeting message.
         """
-        #basic stuff -- getting symbol size, the max amount of symbols the screen can fit, turning text into capitals, setting symbol index to 0
+        # basic stuff -- getting symbol size, the max amount of symbols the screen can fit, turning text into capitals, setting symbol index to 0
         text = text.upper()
-        message = False
 
         start_text_pos_x = x
         start_text_pos_y = y
 
-        #getting the storting position
+        # getting the storting position
         start_pos = (start_text_pos_x, start_text_pos_y)
 
-        #processing the text ig
+        # processing the text ig
         for symbol in text:
             if symbol in self.available_symbols:
                 # the system didn't let me save files named like :.png, etc, so i added this
@@ -96,22 +93,24 @@ class Font:
                     symbol = 'colon'
                 elif symbol == ' ':
                     symbol = 'space'
+                elif symbol == '<':
+                    symbol = 'arrow_left'
+                elif symbol == '>':
+                    symbol = 'arrow_right'
 
                 image = load_image(f'{symbol}.png')
                 # scale the symbol to a new size
                 image = pygame.transform.scale(image, (image.get_width() * size, image.get_height() * size))
 
                 screen.blit(image, start_pos)
-                #change the x
+                # change the x
                 start_text_pos_x += image.get_width() * size + 10
 
             else:
                 self.not_available.add(symbol)
             # change the position and increase the symbol's index by one
             start_pos = (start_text_pos_x, start_text_pos_y)
-
-        self.line = 0
-
+        return start_pos
 
     def start_timer(self, start_time) -> None:
         """
@@ -121,7 +120,6 @@ class Font:
         """
         self.seconds = [str(sec) for sec in range(0, start_time + 1)]
         self.timer_running = True
-
 
     def end_timer(self) -> None:
         """
@@ -136,10 +134,11 @@ class Image(UIElement):
     """
     Class for rendering some imges on the screen
     """
+
     def __init__(self, image: pygame.SurfaceType, x: int, y: int) -> None:
         """
         Creates Image from pygame.Surface
-        
+
         :param image: Surface to use as image
         :param x: x coord of image on screen
         :param y: y coord of image on screen
@@ -147,20 +146,18 @@ class Image(UIElement):
         self.image = image
         self.pos = x, y
         super().__init__()
-    
 
     @classmethod
     def load(cls, filename: str, x: int, y: int) -> "Image":
         """
         Loads and creates an image
-        
+
         :param filename: Image file path
         :param x: x coord of image on screen
         :param y: y coord of image on screen
         :return: New Image object
         """
         return cls(load_image(filename), x, y)
-
 
     def render(self) -> None:
         screen.blit(self.image, self.pos)
@@ -170,90 +167,144 @@ class Video(Image):
     def __init__(self, video: cv2.VideoCapture, x: int, y: int) -> None:
         """
         Create a Video object to show a video on screen
-        
+
         :param video: cv2 VideoCapture object to read frames from
         :param x: x coord of video on screen
         :param y: y coord of video on screen
         """
         self.video = video
-        super().__init__(pygame.Surface((video.get(cv2.CAP_PROP_FRAME_WIDTH), video.get(cv2.CAP_PROP_FRAME_HEIGHT))), x, y)
-    
+        super().__init__(pygame.Surface((video.get(cv2.CAP_PROP_FRAME_WIDTH), video.get(cv2.CAP_PROP_FRAME_HEIGHT))), x,
+                         y)
 
     @classmethod
     def load(cls, filename: str, x: int, y: int) -> "Image":
         """
         Loads and creates a video
-        
+
         :param filename: Video file path
         :param x: x coord of video on screen
         :param y: y coord of video on screen
         :return: New Video object
         """
         return cls(cv2.VideoCapture(filename), x, y)
-    
 
     def read(self) -> pygame.SurfaceType:
         success, frame = self.video.read()
         if not success:
-            return load_image(VIDEO_PLACEHOLDER)
+            return load_image(settings.VIDEO_PLACEHOLDER)
         else:
             return pygame.image.frombuffer(frame.tobytes(), frame.shape[1::-1], "BGR")
-    
 
     def render(self) -> None:
         self.image = self.read()
         return super().render()
-
-
-class Text(UIElement):
-    def __init__(self, text: str, size: int, x: int, y: int) -> None:
-        self.text = text
-        self.size = size
-        self.pos = x, y
-        super().__init__()
-    
-
-    def render(self) -> None:
-        pass
-
 
 class Button(UIElement):
     """
     Creates a button
     Attributes:
         text: The text of the button
-        action: callback funtion to call on click
+        action: callback function to call on click
         x: x coord of left-top corner of the button
         y: y coord of left-top corner of the button
-        width: width of the button
-        height: height of the button
+        sound: true if the sound is played on click
     """
-    def __init__(self, text: str, action: Callable, x: int, y: int, width: int, height: int) -> None:
+
+    def __init__(self, text: str, action: Callable, x: int, y: int, sound: bool) -> None:
+        super().__init__()
         self.text = text
         self.action = action
         self.x, self.y = x, y
-        self.width, self.height = len(text) * 100 * FONT_SIZE, 100
+        self.width, self.height = len(text) * 100 * settings.FONT_SIZE, 100
         self.font = Font()
         self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+        self.sound = sound
+        self.end_pos = (0, 0)
         def callback(e: pygame.event.Event):
-            if self.rect.collidepoint(e.pos[0], e.pos[1]) and self.action:
+            if self.rect.collidepoint(e.pos) and self.action != 'none':
                 self.action()
+                if self.sound:
+                    play_sound('click')
+
         self.listener = ev.add_event_listener(pygame.MOUSEBUTTONDOWN, callback)
-
-        super().__init__()
-
+        self.listeners.append(self.listener)
 
     def render(self) -> None:
         """
         Render the button to the screen
         """
-        pygame.draw.rect(screen, "green", self.rect, border_radius=5)
-        self.font.print_at(self.text, FONT_SIZE, self.x, self.y)
+        #pygame.draw.rect(screen, "green", self.rect, border_radius=5)
+        self.end_pos = self.font.print_at(self.text, settings.FONT_SIZE, self.x, self.y)
+
+class ButtonArrows(Button):
+    """
+    Creates an adjustment button with arrows
+    Attributes:
+        text: The text of the button
+        name: the name of a variable, which will be changed by pressing the arrows
+        value: current value of the variable
+        max_value: max value of the variable
+        x: x coord of left-top corner of the button
+        y: y coord of left-top corner of the button
+    """
+    def __init__(self, text: str, name: str, value: int, max_value: int, x: int, y: int) -> None:
+
+        self.max_value = max_value
+        self.min_value = 1
+        self.name = name
+        self.value = value
+        super().__init__(text, self.apply_value, x, y, False)
+        # note: change the variables to the actual ones
+        self.arrow_left = pygame.Rect(int(self.x + self.width), self.y, self.width, self.height)
+        self.arrow_right = pygame.Rect(int(self.x + self.width + 300), self.y, self.width, self.height)
+
+        def callback(e: pygame.event.Event):
+            if self.arrow_left.collidepoint(e.pos):
+                if self.value != self.min_value:
+                    self.value -= 1
+                    self.action()
+            elif self.arrow_right.collidepoint(e.pos):
+                if self.value != self.max_value:
+                    self.value += 1
+                    self.action()
+
+        self.arrow_listener = ev.add_event_listener(pygame.MOUSEBUTTONDOWN, callback)
+        self.listeners.append(self.arrow_listener)
+
+    def apply_value(self):
+        setattr(settings, self.name, self.value)
+
+    def render(self) -> None:
+        self.end_pos = self.font.print_at(self.text, settings.FONT_SIZE, self.x, self.y)
+        # note: change the variables to the actual ones
+        self.arrow_left = pygame.Rect(self.end_pos[0], self.end_pos[1], 70, self.height)
+        #pygame.draw.rect(screen, "green", self.arrow_left, border_radius=5)
+        self.end_pos = self.font.print_at('<', settings.FONT_SIZE, self.end_pos[0], self.end_pos[1])
+        self.end_pos = self.font.print_at(str(self.value), settings.FONT_SIZE, self.end_pos[0], self.end_pos[1])
+        self.arrow_right = pygame.Rect(self.end_pos[0], self.end_pos[1], 70, self.height)
+        #pygame.draw.rect(screen, "green", self.arrow_right, border_radius=5)
+        self.end_pos = self.font.print_at('>', settings.FONT_SIZE, self.end_pos[0], self.end_pos[1])
 
 
-    def __del__(self) -> None:
-        ev.remove_event_listener(self.listener)
-        super().__del__()
+class Label(UIElement):
+    """
+    Creates a text
+    Attributes:
+        text: The text
+        size: Font size
+        x: x coord of left-top corner of the text
+        y: y coord of left-top corner of the text
+    """
+    def __init__(self, text, size, x, y):
+        self.text = text
+        self.size = size
+        self.x = x
+        self.y = y
+        self.font = Font()
+        super().__init__()
+
+    def render(self):
+        self.font.print_at(self.text, self.size, self.x, self.y)
 
 
 def tick(fps: float = -1) -> float:
@@ -262,6 +313,7 @@ def tick(fps: float = -1) -> float:
         el.render()
     pygame.display.flip()
     return ev.tick(fps)
+
 
 def play_music(name: str) -> None:
     """
@@ -277,6 +329,7 @@ def play_music(name: str) -> None:
         print(f"Cannot load music file: {e}")
     pygame.mixer.music.play(-1)
 
+
 def play_sound(name: str) -> None:
     """
     Plays a sound once
@@ -287,6 +340,7 @@ def play_sound(name: str) -> None:
     full_name = f'sounds/{name}.mp3'
     try:
         sound = pygame.mixer.Sound(full_name)
+        sound.set_volume(settings.VOLUME)
         sound.play()
     except pygame.error as e:
         print(f"Cannot load sound file: {e}")
@@ -297,14 +351,15 @@ def mute_and_unmute_music() -> None:
     Mutes and unmutes the music
     """
     if pygame.mixer.music.get_volume() == 0:
-        pygame.mixer.music.set_volume(VOLUME)
+        pygame.mixer.music.set_volume(settings.VOLUME)
     else:
         pygame.mixer.music.set_volume(0)
+
 
 def bind(key: str, callback: Callable) -> None:
     """
     Binds a function to a key
-    
+
     :param key: Key to bind for
     :type key: str
     :param callback: function to bind
@@ -321,7 +376,7 @@ def bind(key: str, callback: Callable) -> None:
 def unbind(key: str) -> None:
     """
     Unbinds any functions from a key
-    
+
     :param key: Key to unbind
     :type key: str
     """
@@ -330,8 +385,15 @@ def unbind(key: str) -> None:
 
 
 def clearscreen() -> None:
+    """
+    Clears the screen from all the elements
+    :return:
+    """
     global ui_elements
+    for el in ui_elements:
+        el.destroy()
     ui_elements = []
+    screen.fill('black')
 
 
 # setup
